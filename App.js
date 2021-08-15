400552890daf08c4b22a2c4650d5abd42d15645c
KirilDora <78552903+KirilDora@users.noreply.github.com>
2021-08-15 15:22:40 +0300

    Add files via upload
    Version1

---
+++ App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import './App.css';
+import ToDoItem from './ToDoItem/ToDoItem.js';
+import todosData from './todosData.js';
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            todoItems: todosData
+        }
+    }
+    handleChange = id => {
+        const index = this.state.todoItems.map(item => item.id).indexOf(id);
+        this.setState(state => {
+            let { todoItems } = state;
+            todoItems[index].completed = true;
+            return todoItems;
+        })
+    }
+    render() {
+        const { todoItems } = this.state;
+        const activeTasks = todoItems.filter(task => task.completed === false);
+        const completedTasks = todoItems.filter(task => task.completed === true);
+        const finalTasks = [...activeTasks, ...completedTasks].map(item => {
+            return ( <
+                ToDoItem key = { item.id }
+                description = { item.description }
+                completed = { item.completed }
+                handleChange = {
+                    () => { this.handleChange(item.id) } }
+                />
+            )
+        })
+        return ( <
+            div className = "App" > { finalTasks } <
+            /div>
+        );
+    }
+}
+
+export default App;
---
+++ todosData.js
@@ -0,0 +1,28 @@
+const todosData = [{
+        id: 0,
+        description: 'Сходить в магазин',
+        completed: false
+    },
+    {
+        id: 1,
+        description: 'Приготовить обед',
+        completed: false
+    },
+    {
+        id: 2,
+        description: 'Подстричься',
+        completed: false
+    },
+    {
+        id: 3,
+        description: 'Поубирать дома',
+        completed: false
+    },
+    {
+        id: 4,
+        description: 'Выкинуть мусор',
+        completed: false
+    }
+];
+
+export default todosData;
